ee8914ce84cc1e997f62b7a98e0095675b7b3d13
E-RuiL-Vieira <ruiemanuelvieira@usp.br>
2024-08-27 22:09:27 -0300

    Funcao que imprime competicoes

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 import json
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
+import kaggle
+from kaggle.rest import ApiException
+from kaggle.api.kaggle_api_extended import KaggleApi
+from pprint import pprint
 
 # Fiz o bot seguindo um tutorial da biblioteca do telegram mais antiga, 13.13, não funciona na mais nova
 # Vi algo sobre colocar async nas funções, dá pra deixar assim também ou atualizar, vc que sabe
 
-# O que falta fazer: Uma função para mexer com a api do kaggle pelo menos e printar as competições
+api = KaggleApi()
+api.authenticate()
+def competicoes(group = "general", category = "all", sort_by="latestDeadline", page = 1, search=''):
+    try:
+        api_response = api.competitions_list()
+        pprint(api_response)
+    except ApiException as e:
+        print("Exception when calling KaggleApi->competitions_list: %s\n" % e)
+
+
 with open('config.json', 'r') as config_file:
     config = json.load(config_file)
     TOKEN = config['key']
