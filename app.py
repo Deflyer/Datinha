4855105418ef1cc0cc5612edd1d540de17f8ad18
Deflyer <vligmart@usp.br>
2024-09-02 09:38:23 -0300

    Arrumando botoes de prev e next

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler
 import sys
 import os
 from selenium import webdriver
@@ -8,9 +8,13 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
+import kaggle
+from kaggle.rest import ApiException
+from kaggle.api.kaggle_api_extended import KaggleApi
 
 PAGE_SIZE = 5
 competitions = []  # Armazena todas as competições
+competitions_kaggle = [] 
 current_page = 0  # Página atual
 
 def start(update: Update, context: CallbackContext):
@@ -39,53 +43,37 @@ def get_competitions(group = "general", category = "all", sort_by="latestDeadlin
     '''
     Webscrap https://mlcontests.com/#list to get a list of current competitions.
     '''
-    # Configurar o ambiente para o Selenium
     chrome_driver_path = '/usr/lib/chromium-browser/chromedriver'
     chrome_bin_path = '/usr/bin/chromium-browser'
 
-    # Configurar o Selenium para usar o Chrome
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.binary_location = chrome_bin_path
 
-    # Iniciar o WebDriver
     service = Service(executable_path=chrome_driver_path)
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
-    # Navegar para a URL
     url = 'https://mlcontests.com'
     driver.get(url)
-
-    # Esperar que os elementos sejam carregados (ajuste o tempo se necessário)
     driver.implicitly_wait(2)
-
-    # Obtém o HTML da página
     html = driver.page_source
 
-    # Cria o objeto BeautifulSoup com o HTML obtido
     soup = BeautifulSoup(html, 'html.parser')
-
-    # Encontrar o elemento que contém as competições
     contests_div = soup.find(id='contests-cards')
-
-    # Encontrar todas as competições dentro de contests_div
     competitions = contests_div.find_all(class_='card')
 
     competition_list = []
-    # Extrair informações de cada competição
     for competition in competitions:
         card_body = competition.find(class_='card-body')
         card_footer = competition.find(class_='card-footer')
 
-        # Extrair o título, prêmio, organizadores e link
         title = card_body.find(class_='card-title').get_text(strip=True)
         prize = card_body.find(class_='card-subtitle').get_text(strip=True)
         organizers = card_body.find(class_='card-text').get_text(strip=True)
         link = card_body.find(class_='btn-link')['href']
 
-        # Extrair a categoria e o prazo
         category = card_footer.find('span').get_text(strip=True)
         deadline = card_footer.find(class_='text-muted').get_text(strip=True)
         palavras = deadline.split()
@@ -99,42 +87,58 @@ def get_competitions(group = "general", category = "all", sort_by="latestDeadlin
             'category': category,
             'deadline': deadline
         })
-    # Ordenar a lista pela deadline
     competition_list.sort(key=lambda x: x['deadline'])
 
-    # Fechar o WebDriver
     driver.quit()
 
     return competition_list
+def get_competitions_kaggle(group = "general", category = "all", sort_by="latestDeadline", page = 1, search=''):
+    '''
+    Uses the Kaggle API to get a list of current competitions.
+    '''
 
-def create_competition_print_kaggle(c, n_comp, total_comp):
+    try:
+        return api.competitions_list()
+    except ApiException as e:
+        print("Exception when calling KaggleApi->competitions_list: %s\n" % e)
+
+def create_competition_print_kaggle(c):
     '''
     Formats a string with the competition information. Used to get a more beautiful Telegram message.
     '''
     
     response = 'Title: ' + c.title + '\nDescription: ' + c.description + '\nCategory: ' + c.category + '\nReward: ' + c.reward
     response += '\nMax Team Size: ' + str(c.maxTeamSize) + '\nDeadline: ' + str(c.deadline) + '\n' + c.url
-
-    if n_comp+1 < total_comp:
-        response = response + '\n\n----------------------------------------------------\n\n'
         
     return response
     
 
-def kaggle(update: Update, context: CallbackContext):
+def kaggle_func(update: Update, context: CallbackContext):
     '''
     Telegram command to list 5 Kaggle competitions and their information.
     '''
-
-    competitions = get_competitions()
-
-    response = ''
-    number_comp = 5
-
-    for i, comp in enumerate(competitions[:number_comp]):
-        response += create_competition_print_kaggle(comp, i, number_comp)
-
-    update.message.reply_text(response, disable_web_page_preview=True)
+    global current_page
+    current_page = 0
+    start_index = current_page * PAGE_SIZE
+    competitions_kaggle = get_competitions_kaggle()
+    end_index = min(start_index + PAGE_SIZE, len(competitions_kaggle))
+    
+    message = ''
+    for comp in competitions_kaggle[start_index:end_index]:
+        message += create_competition_print_kaggle(comp) + '\n\n'
+    
+    message += '----------------------------------------------------\n\n'
+    keyboard = []
+    if start_index > 0:
+        keyboard.append([InlineKeyboardButton("⬅️ Previous", callback_data=f'prev_{current_page}')])
+    if end_index < len(competitions_kaggle):
+        keyboard.append([InlineKeyboardButton("➡️ Next", callback_data=f'next_{current_page}')])
+    
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    if update.callback_query:
+        update.callback_query.message.edit_text(message, disable_web_page_preview=True, reply_markup=reply_markup)
+    else:
+        update.message.reply_text(message, disable_web_page_preview=True, reply_markup=reply_markup)
 
 def create_competition_print_all(c):
     '''
@@ -149,27 +153,27 @@ def create_competition_print_all(c):
 def send_competitions(update: Update, page: int):
     global competitions
     start_index = page * PAGE_SIZE
-    end_index = min(start_index + PAGE_SIZE, len(competitions))
-    print('hora de pegar')
+
     competitions = get_competitions()
-    print('de alguma forma passei')
+
+    end_index = min(start_index + PAGE_SIZE, len(competitions))
     
     message = ''
     for comp in competitions[start_index:end_index]:
-        print(comp)
         message += create_competition_print_all(comp) + '\n\n'
     
     message += '----------------------------------------------------\n\n'
     keyboard = []
-    print('ue, sai')
     if start_index > 0:
         keyboard.append([InlineKeyboardButton("⬅️ Previous", callback_data=f'prev_{page}')])
     if end_index < len(competitions):
         keyboard.append([InlineKeyboardButton("➡️ Next", callback_data=f'next_{page}')])
     
     reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text(message, disable_web_page_preview=True, reply_markup=reply_markup)
-    #update.message.reply_text(message, parse_mode='Markdown', reply_markup=reply_markup)
+    if update.callback_query:
+        update.callback_query.message.edit_text(message, disable_web_page_preview=True, reply_markup=reply_markup)
+    else:
+        update.message.reply_text(message, disable_web_page_preview=True, reply_markup=reply_markup)
 
 def upcoming(update: Update, context: CallbackContext):
     '''
@@ -209,16 +213,19 @@ with open('config.json', 'r') as config_file:
 updater = Updater(TOKEN)
 dp = updater.dispatcher
 print('Bot linked!')
+dp.add_handler(CallbackQueryHandler(button))
 
 # Accessing Kaggle API.
-
+api = KaggleApi()
+api.authenticate()
 print('Kaggle API accessed!')
 
 # Adding commands to the bot.
 dp.add_handler(CommandHandler("start", start))
 dp.add_handler(CommandHandler("help", help))
-dp.add_handler(CommandHandler("kaggle", kaggle))
+dp.add_handler(CommandHandler("kaggle", kaggle_func))
 dp.add_handler(CommandHandler("upcoming", upcoming))
+dp.add_handler(CallbackQueryHandler(button))
 print('Functionalities linked!')
 
 updater.start_polling()
